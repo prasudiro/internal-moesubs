bb69acf7c6663c5a7f4a9efbd243094c50f31d57
Kurniawan Prasetyo <prassaiyan@gmail.com>
2017-09-25 18:24:16 +0700

    Add moeshops

--- resources/views/html/order/index.blade.php
+++ resources/views/html/order/index.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Login Internal | [Moesubs] Jagonya Ngesub</title>
+    <title>Pre-Order Gantungan Kunci Logo Moesubs | [Moeshops] Jagonya Ngehobi</title>
 
     <link href="{{ URL('css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{ URL('font-awesome/css/font-awesome.css')}}" rel="stylesheet">
